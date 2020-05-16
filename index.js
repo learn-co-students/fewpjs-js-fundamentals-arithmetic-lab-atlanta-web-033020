2cc30b28f93430e83faa89e1c921c739906869fd
rpdecks <rpdecks@gmail.com>
2020-05-15 21:56:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.random() * 10;
+const mod = 16%6
+const max = Math.max(1, 2, 3, 4, 20);
 
-// Write your code here
